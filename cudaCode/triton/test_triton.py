23f59e8de70ab337e20862c50722032f997848e6
kennys <shykennys@gmail.com>
2025-07-13 21:32:16 +0800

    triton add kernel

--- cudaCode/triton/test_triton.py
+++ cudaCode/triton/test_triton.py
@@ -7,6 +7,7 @@ import triton.language as tl
 Demo for triton.
 """
 
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
 
 @triton.jit
 def add_kernel(
@@ -37,6 +38,23 @@ def add(
     """
     output = torch.empty_like(x)
     assert x.is_cuda and y.is_cuda and output.is_cuda
+    n_elements = output.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['block_size']), )
+
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+
+    return output
     
      
-    
+if __name__ == "__main__":
+
+    torch.manual_seed(0)
+    size = 98432
+    x = torch.rand(size, device=DEVICE)
+    y = torch.rand(size, device=DEVICE)
+    output_torch = x + y
+    output_triton = add(x, y)
+    print(output_torch)
+    print(output_triton)
+    print(f'The maximum difference between torch and triton is '
+        f'{torch.max(torch.abs(output_torch - output_triton))}')
