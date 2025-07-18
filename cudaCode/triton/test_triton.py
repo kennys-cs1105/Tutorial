051d8f8635150deacc0bb11a80c979981828963e
shykennys <shykennys@gmail.com>
2025-07-18 16:10:13 +0800

    triton softmax

--- cudaCode/triton/test_triton.py
+++ cudaCode/triton/test_triton.py
@@ -5,11 +5,12 @@ import triton.language as tl
 
 """
 Demo for triton.
-1. vector add
 """
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
+
+# 1. vector add
 @triton.jit
 def add_kernel(
     x_ptr,
@@ -52,7 +53,34 @@ def add(
     add_kernel[grid](x, y, output, n_elements, block_size=1024)
 
     return output
-    
+
+# --------------------------------------------------------------------------------#
+
+# 2. fused-softmax
+@triton.jit
+def softmax_kernel(
+    output_ptr,
+    input_ptr,
+    input_row_stride,
+    output_row_stride,
+    n_rows,
+    n_cols,
+    BLOCK_SIZE:tl.constexpr,
+    num_stages:tl.constexpr
+):
+    row_start = tl.program_id(0)
+    row_step = tl.num_programs(0)
+    for row_idx in tl.range(row_start, n_rows, row_step, num_stages=num_stages):
+        row_start_ptr = input_ptr + row_idx * input_row_stride
+        col_offsets = tl.arange(0, BLOCK_SIZE)
+        input_ptrs = row_start_ptr + col_offsets
+        mask = col_offsets < n_cols
+        row = tl.load(input_ptrs, mask=mask, other=-float("inf"))
+        row_minus_max = row - tl.max(row, axis=0)
+        
+
+
+
      
 if __name__ == "__main__":
 
